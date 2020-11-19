c8cfe684f17498e5b98b9fa60bac27800ffb2f3d
mccray austin <tubach75@hotmail.com>
2020-11-19 19:52:41 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,43 +1,22 @@
 module Players
   require "pry"
   class Computer < Player
-=begin
+
     def move(board)
-      valid_moves = []
+      c = ""
       num = 1
-      #string = "#{num}"
-      board.cells.collect do |cell|
+      board.cells.each do |cell|
         if cell == " "
-          valid_moves << (cell = num.to_s)
+          c = cell + num.to_s
           num += 1
-        #binding.pry
+        break
+        else
+          num += 1
         end
       end
-      valid_moves
-      binding.pry
-    end
-=end
-
-def move(board)
-   c = ""
-   num = 1
-   board.cells.each do |cell|
-     if cell == " "
-       #num += 1
-       c = cell + num.to_s
-       #print c
-       num += 1
-     break
-     else
-       num += 1
-     #num += 1
-     end
-   end
-   c.strip
- end
+    c.strip
+  end
 
   end
 end
 
-#c = Players::Computer.new("X")
-#c.move
